1aa5dcdaceedb49e8c465bf7bccc131ebec6bc2b
ednafleite <ednaflleite@gmail.com>
2022-11-07 22:57:18 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,112 @@
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+
+function HomePage() {
+    const estilosDaHomePage = {
+        // backgroundColor: "red" 
+    };
+
+    // console.log(config.playlists);
+
+    return (
+        <>
+            <CSSReset />
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                // backgroundColor: "red",
+            }}>
+                <Menu />
+                <Header />
+                <Timeline playlists={config.playlists}>
+                    Conteúdo
+                </Timeline>
+            </div>
+        </>
+    );
+}
+
+export default HomePage
+
+// function Menu() {
+//     return (
+//         <div>
+//             Menu
+//         </div>
+//     )
+// }
+
+
+const StyledHeader = styled.div`
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .user-info {
+        margin-top: 50px;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
+`;
+function Header() {
+    return (
+        <StyledHeader>
+            {/* <img src="banner" /> */}
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>
+                        {config.name}
+                    </h2>
+                    <p>
+                        {config.job}
+                    </p>
+                </div>
+            </section>
+        </StyledHeader>
+    )
+}
+
+
+function Timeline(props) {
+    const playlistsNames = Object.keys(props.playlists) //pegar os nome dos arrays, as chaves
+    // Statement
+    // Retorno por expressão é como o react trabalha
+    // => = Dado uma variável, retona ela mesma
+    return (
+        <StyledTimeline>
+            {playlistsNames.map((playlistsNames) => {
+                const videos = props.playlists[playlistsNames] //vai puxar os vídeo de cada playlist pelo nome da playlist
+                return (
+                    <section>
+                        <h2>{config.playlistsNames}</h2>
+                        <div>
+                            {
+                                videos.map((video) => {
+                                    return (
+
+                                        <a href={video.url}>
+                                            <img src={video.thumb} />
+                                            <span>
+                                                {video.title}
+                                            </span>
+                                        </a>
+                                    )
+                                })
+                            }
+                        </div>
+                    </section>
+                )
+
+            })}
+        </StyledTimeline>
+    )
+}
